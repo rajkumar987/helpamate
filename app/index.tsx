79dd50f8d0e7ee437d80b65e088932bd3d370249
rajkumar987 <abothurajkumar1998@gmail.com>
2023-12-18 15:43:52 +0530

    first-commit:initializing

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, Image, Dimensions } from "react-native";
+import Onboarding from "react-native-onboarding-swiper";
+import { Text, View } from "../components/Themed";
+
+const { width, height } = Dimensions.get("window");
+
+const OnBoarding = () => {
+  return <Text>OnBoarding</Text>;
+};
+
+export default OnBoarding;
